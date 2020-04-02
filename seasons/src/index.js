48077d890e07e1745b6d84db82e7a2b65891be43
KellyPT <trankphuong@gmail.com>
2020-04-02 16:22:49 -0700

    avoid conditionals in render() method

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -21,7 +21,7 @@ class App extends React.Component {
     console.log('My component was just updated - it re-rendered!');
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -29,6 +29,10 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
     return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
