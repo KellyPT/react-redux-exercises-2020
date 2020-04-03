d598f22e3871784a06f5b103e7029bb0248da083
KellyPT <trankphuong@gmail.com>
2020-04-03 16:57:43 -0700

    add new project pictures

---
+++ pictures/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+const App = () => {
+  return (
+    <div className="ui container" style={{ marginTop: '10px' }}>
+      <h1>Hello this is my App!</h1>
+      <SearchBar />
+    </div>
+  );
+};
+
+export default App;
