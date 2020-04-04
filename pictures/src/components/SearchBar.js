c7875e5a86cf15ee7249ff7dbe2644440119768f
KellyPT <trankphuong@gmail.com>
2020-04-03 23:46:58 -0700

    play around with execution context

--- pictures/src/components/SearchBar.js
+++ pictures/src/components/SearchBar.js
@@ -1,17 +1,25 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  state = { term: '' };
+
+  onFormSubmit = e => {
+    e.preventDefault(); // prevent the browser from submitting and resetting the form after we hit Enter
+
+    console.log(this.state.term);
+  };
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange}></input>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            ></input>
           </div>
         </form>
       </div>
