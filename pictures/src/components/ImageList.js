d82b5ae7177d24421ebd3d98888bb4fda031b654
KellyPT <trankphuong@gmail.com>
2020-04-05 01:27:23 -0700

    minor tweak for props in img tags

--- pictures/src/components/ImageList.js
+++ pictures/src/components/ImageList.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 const ImageList = props => {
   console.log(props.images);
-  const images = props.images.map(image => {
-    return (
-      <img src={image.urls.regular} key={image.id} alt={image.tags[0].title} />
-    );
-  });
+  const images = props.images.map(
+    ({ description, alt_description, id, urls }) => {
+      return (
+        <img
+          src={urls.regular}
+          key={id}
+          alt={description ? description : alt_description}
+        />
+      );
+    }
+  );
   return <div>{images}</div>;
 };
 
