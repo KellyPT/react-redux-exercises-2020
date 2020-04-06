97d258187dedb03a591ee4b2879a47b5bb5e0173
KellyPT <trankphuong@gmail.com>
2020-04-06 00:14:55 -0700

    display the list of images in grid form and create ImageCard for each image

--- pictures/src/components/ImageList.js
+++ pictures/src/components/ImageList.js
@@ -1,19 +1,13 @@
 import React from 'react';
+import './ImageList.css';
+import ImageCard from './ImageCard';
 
-const ImageList = props => {
+const ImageList = (props) => {
   console.log(props.images);
-  const images = props.images.map(
-    ({ description, alt_description, id, urls }) => {
-      return (
-        <img
-          src={urls.regular}
-          key={id}
-          alt={description ? description : alt_description}
-        />
-      );
-    }
-  );
-  return <div>{images}</div>;
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />;
+  });
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
