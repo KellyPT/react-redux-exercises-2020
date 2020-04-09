9ad7959751ad8ad6a6ddffa0d8a5ba4805412c21
KellyPT <trankphuong@gmail.com>
2020-04-08 17:20:41 -0700

    styling for list of videos

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './VideoItem.css';
 
 const VideoItem = ({ video }) => {
   const snippet = video.snippet;
   return (
-    <div className="item">
+    <div className="video-item item">
       <img
         className="image"
         src={snippet.thumbnails.medium.url}
