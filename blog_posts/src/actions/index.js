1ca027d68ca280760a4d482c56de7c6d6623ec52
KellyPT <trankphuong@gmail.com>
2020-04-18 13:30:05 -0700

    alternative overfetching solution with action creators insinde action creators

--- blog_posts/src/actions/index.js
+++ blog_posts/src/actions/index.js
@@ -1,6 +1,15 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  // make sure network call for fetching all posts finishes first before moving to next logic
+  await dispatch(fetchPosts());
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+
+  // dont need await keyword here because we dont need to wait for all ids to be fetched; async await syntax doesnt work with forEach statement
+  userIds.forEach((id) => dispatch(fetchUser(id)));
+};
+
 export const fetchPosts = () => {
   return async (dispatch) => {
     const response = await jsonPlaceholder.get('/posts');
@@ -11,13 +20,19 @@ export const fetchPosts = () => {
   };
 };
 
-export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
-
-// memoize network requests outside of action creator
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async (dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({
     type: 'FETCH_USER',
     payload: response.data
   });
-});
+};
+
+// First refactor: memoize network requests outside of action creator
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({
+//     type: 'FETCH_USER',
+//     payload: response.data
+//   });
+// });
