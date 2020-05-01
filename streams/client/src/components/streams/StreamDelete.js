ddf53c53f4e071ec6dcdc34656a961a26379fccb
KellyPT <trankphuong@gmail.com>
2020-05-01 12:11:23 -0700

    complete Delete Stream workflow

--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,25 +1,62 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Modal from '../Modal';
 import history from '../../history';
+import { fetchStream, deleteStream } from '../../actions';
 
-const StreamDelete = () => {
-  const actions = (
-    <React.Fragment>
-      <button className="ui button negative">Delete</button>
-      <button className="ui button">Cancel</button>
-    </React.Fragment>
-  );
-  return (
-    <div>
-      StreamDelete
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.streamId);
+  }
+
+  renderActions() {
+    return (
+      <React.Fragment>
+        <button
+          onClick={() => this.props.deleteStream(this.props.streamId)}
+          className="ui button negative"
+        >
+          Delete
+        </button>
+        <Link to="/" className="ui button">
+          Cancel
+        </Link>
+      </React.Fragment>
+    );
+  }
+
+  renderContent() {
+    if (!this.props.stream) {
+      return 'Are you sure you want to delete this stream?';
+    }
+
+    return `Are you sure you want to delete the stream with title: ${this.props.stream.title}?`;
+  }
+
+  render() {
+    return (
       <Modal
         title="Delete Stream"
-        content="Are you sure you want to delete this stream?"
-        actions={actions}
-        onDismiss={() => history.push('/')}
+        content={this.renderContent()}
+        actions={this.renderActions()}
+        onDismiss={() => {
+          console.log('dismiss in action');
+          history.push('/');
+        }}
       />
-    </div>
-  );
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  const streamId = ownProps.match.params.id;
+  return {
+    stream: state.streams[streamId],
+    streamId: streamId
+  };
 };
 
-export default StreamDelete;
+export default connect(mapStateToProps, { fetchStream, deleteStream })(
+  StreamDelete
+);
