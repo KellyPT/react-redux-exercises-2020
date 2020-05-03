9b1a8809aaf8d1c1c2a24899d935857d6b3a9563
KellyPT <trankphuong@gmail.com>
2020-05-02 19:05:26 -0700

    build video player with flv library

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import flv from 'flv.js'; // download video streams and serve that data to our app interface
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
+
+  // this function will run only once so we need to handle the case when component finished mounting before stream data has been loaded to Redux store
   componentDidMount() {
-    this.props.fetchStream(this.props.streamId);
+    const { streamId } = this.props;
+    this.props.fetchStream(streamId);
+    this.buildPlayer();
+  }
+
+  // call buildPlayer() again in case it takes a while for stream data to be loaded into Redux store
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    const { streamId } = this.props;
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${streamId}.flv` // url of node media server we set up earlier
+    });
+
+    this.player.attachMediaElement(this.videoRef.current); // be careful of edge case when stream hasn't been loaded to our Redux store and therefore no video element has been rendered
+    this.player.load();
   }
 
   render() {
@@ -16,6 +44,11 @@ class StreamShow extends React.Component {
 
     return (
       <div>
+        <video
+          ref={this.videoRef}
+          styleprops={{ width: '100%' }}
+          controls={true}
+        />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
