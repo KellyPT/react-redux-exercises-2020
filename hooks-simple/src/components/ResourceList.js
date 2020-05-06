9a249c53fbad149b861f958f4c4d377dcbeba3e4
KellyPT <trankphuong@gmail.com>
2020-05-05 17:05:11 -0700

    extract Hooks logic to a separate file for reusability

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,20 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import useResources from './useResource';
 
 const ResourceList = ({ resourceType }) => {
-  const [resources, setResources] = useState([]);
-  const fetchResource = async (resourceType) => {
-    const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/${resourceType}`
-    );
-    setResources(response.data);
-  };
-
-  useEffect(() => {
-    fetchResource(resourceType);
-  }, [resourceType]);
-
-  return <div>{resources.length}</div>;
+  const resources = useResources(resourceType);
+  return (
+    <div>
+      {resources.map((record) => (
+        <li key={record.id}>{record.title}</li>
+      ))}
+    </div>
+  );
 };
 
 export default ResourceList;
